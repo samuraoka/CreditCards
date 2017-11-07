84145c190172508c8ee23592e0366888497b5d02
Satoshi Muraoka <sa.muraoka@outlook.com>
2017-11-07 20:53:50 +0900

    created migration data implemented controllers implemented view models implemented views

---
+++ CreditCards.Data/Migrations/20171107105955_Init.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CreditCards.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CreditCardApplication",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GrossAnnualIncome = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CreditCardApplication", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CreditCardApplication");
+        }
+    }
+}
---
+++ CreditCards.Web/Controllers/ApplyController.cs
@@ -0,0 +1,51 @@
+﻿using CreditCards.Core.Interface;
+using CreditCards.Core.Model;
+using CreditCards.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CreditCards.Web.Controllers
+{
+    public class ApplyController : Controller
+    {
+        private readonly ICreditCardApplicationRepository _applicationRepository;
+
+        public ApplyController(ICreditCardApplicationRepository applicationRepository)
+        {
+            _applicationRepository = applicationRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(NewCreditCardApplicationDetails applicationDetails)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return View(applicationDetails);
+            }
+
+            var creditCardApplication = new CreditCardApplication
+            {
+                FirstName = applicationDetails.FirstName,
+                LastName = applicationDetails.LastName,
+                Age = applicationDetails.Age.Value,
+                GrossAnnualIncome = applicationDetails.GrossAnnualIncome.Value,
+            };
+
+            await _applicationRepository.AddAsync(creditCardApplication);
+
+            //TODO consider Post-Redirect-Get pattern
+            return View("ApplicationComplete", creditCardApplication);
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
