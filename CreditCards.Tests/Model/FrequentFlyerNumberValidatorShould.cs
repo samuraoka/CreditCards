ae3765e9cda72008f029dfcfecfbeb9efe6b591a
Satoshi Muraoka <sa.muraoka@outlook.com>
2017-11-29 08:10:19 +0900

    added some test methods Adding Additional FrequentFlyerNumberValidator Tests

--- CreditCards.Tests/Model/FrequentFlyerNumberValidatorShould.cs
+++ CreditCards.Tests/Model/FrequentFlyerNumberValidatorShould.cs
@@ -1,4 +1,5 @@
 using CreditCards.Core.Model;
+using System;
 using Xunit;
 
 namespace CreditCards.Tests.Model
@@ -16,12 +17,54 @@ namespace CreditCards.Tests.Model
             Assert.True(sut.IsValid(number));
         }
 
-        [Fact]
-        public void NotAcceptValidSchemes()
+        [Theory]
+        [InlineData("012345-1")]
+        [InlineData("012345-B")]
+        [InlineData("012345-P")]
+        [InlineData("012345-R")]
+        [InlineData("012345-X")]
+        [InlineData("012345-Z")]
+        [InlineData("012345- ")]
+        [InlineData("012345-a")]
+        [InlineData("012345-q")]
+        [InlineData("012345-y")]
+        public void RejectInvalidSchemes(string number)
         {
             var sut = new FrequentFlyerNumberValidator();
 
-            Assert.False(sut.IsValid("012345-B"));
+            Assert.False(sut.IsValid(number));
+        }
+
+        [Theory]
+        [InlineData("0012345-A")]
+        [InlineData("X12345-A")]
+        [InlineData("01234X-A")]
+        public void RejectInvalidMemberNumbers(string number)
+        {
+            var sut = new FrequentFlyerNumberValidator();
+
+            Assert.False(sut.IsValid(number));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("      -A")]
+        [InlineData("  1   -A")]
+        [InlineData("1     -A")]
+        [InlineData("     1-A")]
+        public void RejectEmptyMemberNumberDigits(string number)
+        {
+            var sut = new FrequentFlyerNumberValidator();
+
+            Assert.False(sut.IsValid(number));
+        }
+
+        [Fact]
+        public void ThrowExceptionWhenNullFrequentFlyerNumber()
+        {
+            var sut = new FrequentFlyerNumberValidator();
+
+            Assert.Throws<ArgumentNullException>(() => sut.IsValid(null));
         }
     }
 }
