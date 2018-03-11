46a931c2dfc5b5cf9acda52b39fc78a9e73d656f
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-03-11 11:48:52 +0900

    Added a test class for ApplyController.

---
+++ CreditCards.Tests/Controllers/ApplyControllerShould.cs
@@ -0,0 +1,34 @@
+﻿using CreditCards.Core.Interface;
+using CreditCards.Web.Controllers;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace CreditCards.Tests.Controllers
+{
+    public class ApplyControllerShould
+    {
+        private readonly Mock<ICreditCardApplicationRepository> mockRepository;
+        private readonly Mock<IDataProtectionProvider> mockDataProtectionProvider;
+        private readonly ApplyController sut;
+
+        public ApplyControllerShould()
+        {
+            // Arrange
+            mockRepository = new Mock<ICreditCardApplicationRepository>();
+            mockDataProtectionProvider = new Mock<IDataProtectionProvider>();
+            sut = new ApplyController(mockRepository.Object, mockDataProtectionProvider.Object);
+        }
+
+        [Fact]
+        public void ReturnViewForIndex()
+        {
+            // Act
+            IActionResult result = sut.Index();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
